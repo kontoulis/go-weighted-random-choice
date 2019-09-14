94de9441c24efbea055ea738df3813195bd10c15
Asmodai <kontoulisdhm@gmail.com>
2019-09-14 20:44:03 +0300

    Initial Commit

---
+++ WeightedRandomChoice.go
@@ -0,0 +1,73 @@
+package WeightedRandomChoice
+
+import (
+	"math/rand"
+	"sort"
+	"time"
+)
+
+type WeightedRandomChoice struct {
+	Elements          [] string
+	Weights           [] int
+	TotalWeight       int
+	calibratedWeights bool
+	precision         int
+	calibrateValue    int
+}
+
+func (wrs *WeightedRandomChoice) AddElement(element string, weight int) {
+	weight *= wrs.calibrateValue
+	i := sort.Search(len(wrs.Weights), func(i int) bool { return wrs.Weights[i] > weight })
+	wrs.Weights = append(wrs.Weights, 0)
+	wrs.Elements = append(wrs.Elements, "")
+	copy(wrs.Weights[i+1:], wrs.Weights[i:])
+	copy(wrs.Elements[i+1:], wrs.Elements[i:])
+	wrs.Weights[i] = weight
+	wrs.Elements[i] = element
+	wrs.TotalWeight += weight
+}
+
+func (wrs *WeightedRandomChoice) AddElements(elements map[string]int) {
+	for element, weight := range elements{
+		wrs.AddElement(element, weight)
+	}
+}
+
+func (wrs *WeightedRandomChoice) GetRandomChoice() string {
+	rand.Seed(time.Now().UnixNano())
+	value := rand.Intn(wrs.TotalWeight)
+	if !wrs.calibratedWeights {
+		wrs.calibrateWeights()
+	}
+	for key, weight := range wrs.Weights {
+		value -= weight
+		if value <= 0 {
+			return wrs.Elements[key]
+		}
+	}
+	return ""
+}
+
+func (wrs *WeightedRandomChoice) calibrateWeights() {
+	if wrs.TotalWeight/wrs.precision < 1 {
+		wrs.calibrateValue = wrs.precision / wrs.TotalWeight
+		wrs.TotalWeight = 0
+		for key := range wrs.Weights {
+			wrs.Weights[key] *= wrs.calibrateValue
+			wrs.TotalWeight += wrs.Weights[key]
+		}
+		wrs.calibratedWeights = true
+	}
+}
+
+func New(precision ...int) WeightedRandomChoice {
+	var p = 1000
+	if len(precision) > 0 {
+		p = precision[0]
+	}
+	return WeightedRandomChoice{
+		precision:         p,
+		calibratedWeights: false,
+		calibrateValue:    1,
+	}
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Weighted Random Choice
+This is a simple package for WR choices based on weights.
+
+### Usage
+Instantiate a `WeightedRandomChoice` object by calling `New()`.
+`New()` takes optional argument `precision` which is by default `1000`.
+This is used to calibrate the weights for more precise results
+Add elements with `name` and `weight` and then call `GetRandomChoice` to retrieve an element.
+```go
+package main
+
+import (
+	"WeightedRandomChoice"
+)
+func main(){
+    wrs := WeightedRandomChoice.New()
+    // Add elements one by one 
+    wrs.AddElement("common", 59)
+    wrs.AddElement("epic", 1)
+    wrs.AddElement("rare", 10)
+    wrs.AddElement("green", 30)
+    // OR add elements as a map[string]int
+     wrs.AddElements(map[string]int{
+    		"common"    : 59,
+    		"epic"      : 1,
+    		"rare"      : 10,
+    		"green"     : 30,
+    	})
+   
+    choice := wrs.GetRandomChoice()
+}
+```
